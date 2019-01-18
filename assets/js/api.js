5fe485069dda45e5aabf6adfce6f8833c3a2f6c8
Ferat Imrak <feratimrak1@gmail.com>
2019-01-17 23:18:22 -0600

    work on js

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,40 +1,52 @@
 
-$(document).ready(function(){
-var APIkey = "5Nnteu5jva3e34yiO4plyxydSMn8vkFs";
+$(document).ready(function () {
 
-var queryURL = "https://api.giphy.com/v1/gifs/search?q=ryan+gosling&api_key=" + APIkey + "&limit=5";
 
-$.ajax({
-    url: queryURL,
-    method: "GET"
-}).then(function (response) {
-    console.log(response);
-})
+    var topics = ['hours', 'dog', 'cat', 'lion', 'bird', 'butterfly', 'chicken', 'goldfish', 'gerbil'];
 
-var topics = ['hours', 'dog', 'cat', 'lion', 'bird', 'butterfly', 'chicken', 'goldfish', 'gerbil'];
-console.log(topics);
+    function buttons() {
+        for (var i = 0; i < topics.length; i++) {
+            //console.log(topics[i]);
+            var button = $("<button>").addClass("btn");
+            button.text(topics[i]);
+            $("#topics").append(button);
 
-function buttons() {
-    for (var i = 0; i < topics.length; i++) {
-        //console.log(topics[i]);
-       var button = $("<button>").addClass("btn");
-       button.text(topics[i]);
+
+        }
+    }
+
+    buttons();
+
+    $(".btn").click(function () {
+        event.preventDefault();
+        var gifAnimal = $(this).val().trim();
+        console.log(gifAnimal);
+        var APIkey = "5Nnteu5jva3e34yiO4plyxydSMn8vkFs";
+
+        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + gifAnimal + "&api_key=" + APIkey + "&limit=5";
+
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        }).then(function (response) {
+            console.log(response);
+        })
+
+        //buttons();
+    })
+
+    $("#add-animal").click(function () {
+        //when submit button click add another button in div
+        event.preventDefault();
+        var animal = $("#animal-input").val().trim();
+        topics.push(animal);
+        console.log(animal);
+        var button = $("<button>").addClass("btn");
+        button.text(animal);
         $("#topics").append(button);
 
+    })
 
-    }
-}
-buttons();
-$(".btn").click(function () {
-    //console.log("I am a button");
-    //event.preventDefault();
-    var animal = $("#animal-input").val();
-
-    topics.push(animal);
-    console.log(animal);
-   // buttons();
-
-})
 })
 
 //create an onclick function grab the value of the button
